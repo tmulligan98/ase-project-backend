91d8520468b6898946edc1f91dfbfbb34d9b3f8b
Cormac Doyle <doylec54@tcd.ie>
2022-01-28 12:07:23 +0000

    fixed cors issue

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from backend.routers import sample_router
+from fastapi.middleware.cors import CORSMiddleware
 
 API_VERSION_PREFIX = "/api/1"
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def hello_world():
