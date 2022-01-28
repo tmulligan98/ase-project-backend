a83ceed4a8a79d62a0ca5e53141273f2d04b79bf
Cormac Doyle <doylec54@tcd.ie>
2022-01-28 12:12:59 +0000

    formatting issues

--- backend/main.py
+++ backend/main.py
@@ -6,18 +6,18 @@ API_VERSION_PREFIX = "/api/1"
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "localhost:3000"
-]
+
+origins = [ "http://localhost:3000",  "localhost:3000"]
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
+
 
 @app.get("/")
 def hello_world():
