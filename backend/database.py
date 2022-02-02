ca35f1f9b45972033cad8580aefd8df54d79e80c
conoroheocha <conoroheocha@gmail.com>
2022-02-02 16:08:45 +0000

    sorta working root user

--- backend/database.py
+++ backend/database.py
@@ -4,12 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_NAME = "mysql_db"
 PASSWORD = "root"
-USER = "user"
+USER = "root"
 HOST = "db"
 
-SQLALCHEMY_DATABASE_URL = (
-    f"mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
-)
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
