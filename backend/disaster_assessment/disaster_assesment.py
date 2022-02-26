e009b29d5e7f4dde016058d8ece8bf22823a60af
Ashutosh <ashutoshb2000@gmail.com>
2022-02-26 14:55:02 +0000

    added coordinates, example data file

--- backend/disaster_assessment/disaster_assesment.py
+++ backend/disaster_assessment/disaster_assesment.py
@@ -1,5 +1,7 @@
 import haversine as hs
 
+import json
+
 
 def get_nearest_services(disasters, emergency_services):
     data_to_return = {}
@@ -68,31 +70,61 @@ def get_nearest_services(disasters, emergency_services):
             for es in es_of_consideration:  # type: ignore
                 x = {}
                 for es_dict in distributed_es[es]:  # type: ignore
-                    x[es_dict["name"]] = hs.haversine(
-                        (disaster["lat"], disaster["long"]),
-                        (es_dict["lat"], es_dict["long"]),
+                    x[es_dict["name"]] = (
+                        hs.haversine(
+                            (disaster["lat"], disaster["long"]),
+                            (es_dict["lat"], es_dict["long"]),
+                        ),
+                        es_dict["lat"],
+                        es_dict["long"],
                     )
                 all_services[es] = x
 
-            print(f"all services ==> {all_services}")
-
             for es_name, distances in all_services.items():
                 for name, distance in distances.items():
-                    if distance == sorted(list(distances.values()))[0]:
-                        first_nearest_services[es_name] = {name: distance}
-                    if distance == sorted(list(distances.values()))[1]:
-                        second_nearest_services[es_name] = {name: distance}
-                    if distance == sorted(list(distances.values()))[2]:
-                        third_nearest_services[es_name] = {name: distance}
-
-            print(f"first nearest ==>{first_nearest_services}")
-            print(f"second nearest ==>{second_nearest_services}")
-            print(f"third nearest ==>{third_nearest_services}")
+                    if (
+                        distance
+                        == sorted(list(distances.values()), key=lambda t: t[0])[0]
+                    ):
+                        first_nearest_services[es_name] = {
+                            name: {
+                                "distance": distance[0],
+                                "lat": distance[1],
+                                "long": distance[2],
+                            }
+                        }
+                    if (
+                        distance
+                        == sorted(list(distances.values()), key=lambda t: t[0])[1]
+                    ):
+                        second_nearest_services[es_name] = {
+                            name: {
+                                "distance": distance[0],
+                                "lat": distance[1],
+                                "long": distance[2],
+                            }
+                        }
+                    if (
+                        distance
+                        == sorted(list(distances.values()), key=lambda t: t[0])[2]
+                    ):
+                        third_nearest_services[es_name] = {
+                            name: {
+                                "distance": distance[0],
+                                "lat": distance[1],
+                                "long": distance[2],
+                            }
+                        }
 
             data_to_return[disaster_id] = {
                 "first nearest": first_nearest_services,
                 "second nearest": second_nearest_services,
                 "third nearest": third_nearest_services,
             }
+
+    json_object = json.dumps(data_to_return, indent=4)
+
+    with open("return.json", "w") as outfile:
+        outfile.write(json_object)
 
     return data_to_return
